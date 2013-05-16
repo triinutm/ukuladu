3358cc8d728c1a85466f786444a2b83f90f2d93a
ukulahe@gmail.com <ukulahe@gmail.com@ced98485-01de-99dd-942f-6b1221a858f3>
2013-05-16 15:05:21 +0000

    Edasi 2

--- trunk/Ladu/src/model/PWDecoder.java
+++ trunk/Ladu/src/model/PWDecoder.java
@@ -1,10 +1,26 @@
 package model;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class PWDecoder {
 
-	public String hashPassword(String password) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
+	 public String hashPassword(String password){
+		  String hashedPassword=null;
+		  if(password==null) return null;
+		  try {
+		   MessageDigest digest=MessageDigest.getInstance("MD5");
+		   digest.update(password.getBytes(), 0, password.length());
+		   hashedPassword=new BigInteger(1,digest.digest()).toString(16);
+		   
+		  } catch (NoSuchAlgorithmException e) {
+		   e.printStackTrace();
+		   
+		  }
+		  
+		  return hashedPassword;
+		 }
 
 }
